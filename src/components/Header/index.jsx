beb01cd223f9ec911c41a4a10f9c21ba11578157
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2022-10-13 13:07:41 -0300

    Added header texts

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,11 @@
+import { HeaderTexts } from "../HeaderTexts"
 import { Socials } from "../Socials"
 
 export const Header = () => {
     return (
       <header>
         <Socials />
+        <HeaderTexts />
       </header>
     )
   }
---
+++ src/components/HeaderTexts/index.jsx
@@ -0,0 +1,7 @@
+export const HeaderTexts = () => {
+    return (
+        <div className="header-texts">
+            <p>Made with 💙 by Emilyn C. Pieritz</p>
+        </div>
+    )
+  }
